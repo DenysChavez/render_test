f0f10c994e7587776eb52a20b25917c99d6d8c06
Denys <go4denys@gmail.com>
2023-07-31 10:47:46 -0400

    -add build file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const app = express();
 
 app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
